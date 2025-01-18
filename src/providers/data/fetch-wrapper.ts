58af30b5e3eb301bd6d7a9525b09e67c23489ac2
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2025-01-18 22:43:04 +0530

    setup of providers

---
+++ src/providers/data/fetch-wrapper.ts
@@ -0,0 +1,24 @@
+const customFetch = async (url: string, options: RequestInit) => {
+  const accessToken = localStorage.getItem("access_token");
+
+  const headers = options.headers as Record<string, string>;
+
+  return await fetch(url, {
+    ...options,
+    headers: {
+      ...headers,
+      Authorization: headers?.Authorization || `Bearer ${accessToken}`,
+      "Content-Type": "application/json",
+      "Apollo-Require-Preflight": "true",
+    },
+  });
+};
+
+const getGraphQLErrors = (body: Record<"errors", GraphQLFormattedError[] | undefined>):
+Error | null => {
+    if(!body){
+        return{
+            message: 'Unknown error'
+        }
+    }
+}
